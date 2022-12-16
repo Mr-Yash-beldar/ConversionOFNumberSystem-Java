b2fd9ae7a5fda0cdf57542c054f9a18dc1544ee4
Yash-Beldar <yashodip2003beldar@gmail.com>
2022-12-16 17:19:53 +0530

    Conversion of decimal to octal code

---
+++ ConvDec2Oct.java
@@ -0,0 +1,26 @@
+// java program to convert decimal to octal
+
+import java.util.*;
+
+public class ConvDec2Oct {
+  public static void main(String args[]) {
+    int num, counter = 0;
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter any integer number: ");
+    num = sc.nextInt();
+
+    //to store maximum 32 digits
+    int octalVal[] = new int[32];
+
+    while (num > 0) {
+      octalVal[counter++] = num % 8;
+      num = num / 8;
+    }
+
+    /*print octal values stored in octalVal*/
+    for (int i = counter - 1; i >= 0; i--) {
+      System.out.print(octalVal[i]);
+    }
+  }
+}
